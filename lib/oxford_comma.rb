6180b8228e7f80d281fd5602329710ca45dba094
Alex Hare <alexandra.f.hare@gmail.com>
2019-04-08 20:22:05 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,15 +1,10 @@
 def oxford_comma(array)
-
-end
-
-def oxford_comma(array)
-  if array.length == 2
-    array.join(" and ")
-  elsif 2 < array.length
+  if array.length == 2 
+    array.join (" and ")
+  elsif array.length > 2 
     array[-1].insert(0, "and ")
     array.join(", ")
-    #so insert(index_to_add_to, thing_youre_adding)
-  elsif array.length == 1
+  else
     array.join
   end
 end
