5db68a4e7d942e49d0d202a6f529371fc4f49fca
Alex Hare <alexandra.f.hare@gmail.com>
2019-04-08 20:17:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
 
 end
+
+def oxford_comma(array)
+  if array.length == 2
+    array.join(" and ")
+  elsif 2 < array.length
+    array[-1].insert(0, "and ")
+    array.join(", ")
+    #so insert(index_to_add_to, thing_youre_adding)
+  elsif array.length == 1
+    array.join
+  end
+end
